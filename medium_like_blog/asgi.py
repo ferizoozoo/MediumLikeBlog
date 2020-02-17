227f7a33ab8e1a344983921aeafa8653c42ebfd4
ferizoozoo <farhadzohoorz@gmail.com>
2020-02-17 15:09:44 +0330

    Create the initial project skeleton

---
+++ medium_like_blog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for medium_like_blog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium_like_blog.settings')
+
+application = get_asgi_application()
