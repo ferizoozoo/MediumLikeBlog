5b3d347d3ef487bd54cfe01a2091283251fe0ced
ferizoozoo <farhadzohoorz@gmail.com>
2020-02-26 11:05:11 +0330

    Add the skeleton of the views for user post interactions

---
+++ user/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Views for the user interactions with the posts.
+def post(request):
+    pass
+
+def delete(request):
+    pass
+
+def edit(request):
+    pass
+
+def update(request):
+    pass
