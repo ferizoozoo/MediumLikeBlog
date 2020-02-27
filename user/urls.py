e8b5a52de3a7ed17fda7344062798421dac501fa
ferizoozoo <farhadzohoorz@gmail.com>
2020-02-27 15:31:22 +0330

    Add the templates and static files for user post interactions

--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('post/', post, name='post'),
-    path('delete/', delete, name='delete'),
-    path('edit/', edit, name='edit'),
-    path('update/', update, name='update')
-]
