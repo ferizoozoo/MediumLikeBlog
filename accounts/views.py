68db6b9a84272cecddb61043dc1ac1ae7eb30d9f
ferizoozoo <farhadzohoorz@gmail.com>
2020-03-13 22:17:23 +0330

    Extend the UserCreationForm for saving password of the user

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationForm
 from django.urls import reverse_lazy
 
 # Create your views here.
